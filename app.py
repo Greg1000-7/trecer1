204c0ec6cbd91193cd734e41d997c0d2d47fc575
Greg1000-7 <deva.grisha@gmail.com>
2025-05-15 23:45:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import requests
 import json
 import os
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 app = Flask(__name__)
 
 IPINFO_TOKEN = os.getenv("IPINFO_TOKEN", "")
